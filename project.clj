497bfa68344329ee74fa84f3941d1055acb74779
legkvla <vladimir.legkunets@gmail.com>
2019-01-14 21:10:27 +0300

    Adding schema for action handler

--- project.clj
+++ project.clj
@@ -7,6 +7,13 @@
     [org.clojure/clojure "1.8.0"]
     [clj-http "2.3.0"]
     [cheshire "5.5.0"]
+    [http-kit "2.1.19"]
+    [com.stuartsierra/component "0.3.1"]
+    [reloaded.repl "0.2.2"]
+    [metosin/palikka "0.5.1"]
+    [metosin/kekkonen "0.3.2"]
+    [environ "1.0.0"]
+    [org.clojure/tools.trace "0.7.9"]
     ]
   :main ^:skip-aot helda-owgame.core
   :target-path "target/%s"
---
+++ src/helda_owgame/handlers/schema.clj
@@ -0,0 +1,27 @@
+(ns helda-owgame.handlers.schema
+  (:require
+    [schema.core :as s]
+    )
+  )
+
+(s/defschema Entity {
+    :world s/Keyword
+    :model s/Keyword
+    :tags [s/Keyword]
+    (s/optional-key :description) s/Str
+
+    :attrs {s/Keyword s/Any}
+
+    :id s/Str
+    (s/optional-key :parent-id) s/Str
+  })
+
+(s/defschema ActionEvent {
+  :source-entity Entity
+  :target-entity Entity
+  (s/optional-key :action-ctx) {s/Keyword Entity}
+  })
+
+(s/defschema ActionResponse {
+  :action-ctx {s/Keyword Entity} ;updated entities
+  })
