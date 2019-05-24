ffd6cfb0ef4ca93e4cff9f6096511590c1fda27e
legkvla <vladimir.legkunets@gmail.com>
2019-05-24 10:38:29 +0300

    Add maps dsl

---
+++ src/helda_owgame/client/maps.clj
@@ -0,0 +1,26 @@
+(ns helda-owgame.client.maps
+  (:require
+    [helda-owgame.client.entities :refer [save-entity]]
+    )
+  )
+
+(defn init-room-map [id descr legend backgrounds & tiles] {
+  :description descr
+  :attrs {
+    :name id
+    :legend legend
+    :backgrounds backgrounds
+    :tiles tiles
+    :geo-objects []
+    }
+  :tags [id]
+  :actions {}
+  })
+
+(defn add-geo-object [target-map geo]
+  (update-in target-map [:attrs :geo-objects] conj geo)
+  )
+
+(defn save-room-map [world room-map]
+  (save-entity world :helda.RoomMap room-map)
+  )
